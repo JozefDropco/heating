ce303c373ec7ca481a71450184bd2f03aed06c82
JozefDropco <jozef.dropco@gmail.com>
2019-06-19 20:28:50 +0200

    fix

--- src/main/java/org/dropco/smarthome/Main.java
+++ src/main/java/org/dropco/smarthome/Main.java
@@ -2,6 +2,7 @@ package org.dropco.smarthome;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Sets;
 import com.pi4j.component.temperature.TemperatureSensor;
 import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
@@ -15,20 +16,19 @@ import org.dropco.smarthome.gpioextension.ExtendedPin;
 import org.dropco.smarthome.heating.HeatingDao;
 import org.dropco.smarthome.heating.HeatingRefCode;
 import org.dropco.smarthome.heating.HeatingWorker;
+import org.dropco.smarthome.solar.SolarMain;
 import org.dropco.smarthome.solar.SolarSystemDao;
 import org.dropco.smarthome.solar.SolarSystemScheduler;
 import org.dropco.smarthome.solar.move.SafetySolarPanel;
 import org.dropco.smarthome.solar.move.SolarPanelMover;
 import org.dropco.smarthome.watering.WateringJob;
+import org.dropco.smarthome.watering.WateringMain;
 import org.dropco.smarthome.watering.WateringScheduler;
 import org.dropco.smarthome.watering.WateringThreadManager;
 import org.dropco.smarthome.watering.db.WateringDao;
 import org.dropco.smarthome.web.WebServer;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -42,19 +42,23 @@ public class Main {
 
 
     private static final SettingsDao settingsDao = new SettingsDao();
-    private static final GpioController gpio = GpioFactory.getInstance();
     private static Map<String, GpioPinDigitalInput> inputMap = Collections.synchronizedMap(new HashMap<>());
     private static Map<String, GpioPinDigitalOutput> outputMap = Collections.synchronizedMap(new HashMap<>());
 
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws Exception {
         // Create JAX-RS application.
 
         WebServer webServer = new WebServer();
         webServer.start();
+        Set<String> inputs = Sets.newHashSet(args);
+        if (!inputs.contains("--noWatering")){
+            WateringMain.main(settingsDao,inputMap,outputMap,args);
+        }
 
-        //heaterThread.start();
+        if (!inputs.contains("--noSolar")){
+            SolarMain.main(settingsDao,outputMap,args);
+        }
         webServer.join();
     }
 
