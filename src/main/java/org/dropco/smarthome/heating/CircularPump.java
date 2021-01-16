688ecf7f02c392a2f1fa4527726a73d42aabf8ce
Jozef Dropco <Jozef_Dropco@swissre.com>
2021-01-16 13:28:26 +0100

    pumpa

--- src/main/java/org/dropco/smarthome/heating/CircularPump.java
+++ src/main/java/org/dropco/smarthome/heating/CircularPump.java
@@ -12,6 +12,7 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.logging.Logger;
 
 public class CircularPump implements Runnable {
     protected static final String CIRCULAR_PUMP_DIFF_START_TEMP = "CIRCULAR_PUMP_DIFF_START_TEMP";
@@ -20,6 +21,7 @@ public class CircularPump implements Runnable {
 
     static final String T1_TEMP_KEY = "SOLAR";
     static final String T2_TEMP_KEY = "TA3";
+    public static final Logger LOGGER = Logger.getLogger(CircularPump.class.getName());
 
     static AtomicBoolean state = new AtomicBoolean(false);
     AtomicDouble tempT1 = new AtomicDouble(0);
@@ -28,7 +30,8 @@ public class CircularPump implements Runnable {
     private BiConsumer<String, Boolean> commandExecutor;
     private static List<Consumer<Boolean>> subscribers = Lists.newArrayList();
     private SettingsDao settingsDao;
-    public CircularPump(SettingsDao settingsDao,BiConsumer<String, Boolean> commandExecutor) {
+
+    public CircularPump(SettingsDao settingsDao, BiConsumer<String, Boolean> commandExecutor) {
         this.commandExecutor = commandExecutor;
         ServiceMode.addSubsriber(mode -> {
             if (state.get() && mode) {
@@ -42,17 +45,22 @@ public class CircularPump implements Runnable {
     public void run() {
         TempService.subscribe(getDeviceId(T1_TEMP_KEY), value1 -> {
             tempT1.set(value1);
+            LOGGER.info(T1_TEMP_KEY + " teplata je " + value1);
             update.release();
         });
         TempService.subscribe(getDeviceId(T2_TEMP_KEY), value -> {
             tempT2.set(value);
+            LOGGER.info(T2_TEMP_KEY + " teplata je " + value);
             update.release();
         });
         tempT1.set(TempService.getTemperature(getDeviceId(T1_TEMP_KEY)));
         tempT2.set(TempService.getTemperature(getDeviceId(T2_TEMP_KEY)));
+        LOGGER.info(T1_TEMP_KEY + " teplata je " + tempT1.get());
+        LOGGER.info(T2_TEMP_KEY + " teplata je " + tempT2.get());
         while (true) {
             if (!ServiceMode.isServiceMode()) {
                 double difference = tempT1.get() - tempT2.get();
+                LOGGER.info("Rozdiel teplot pre obehové čerpadlo je" + difference);
                 if (difference >= getStartThreshold() && state.compareAndSet(false, true)) {
                     raiseChange(true);
                 }
@@ -65,11 +73,12 @@ public class CircularPump implements Runnable {
     }
 
     void raiseChange(boolean b) {
+        LOGGER.info("Čerpadlo sa "+ (b?"zapne":"vypne"));
         commandExecutor.accept(CIRCULAR_PUMP_PORT, b);
         subscribers.forEach(consumer -> consumer.accept(b));
     }
 
-    void setState(boolean newState){
+    void setState(boolean newState) {
         state.set(newState);
     }
 
@@ -86,7 +95,7 @@ public class CircularPump implements Runnable {
         return settingsDao.getDouble(CIRCULAR_PUMP_DIFF_START_TEMP);
     }
 
-    public static void addSubscriber(Consumer<Boolean> subscriber){
+    public static void addSubscriber(Consumer<Boolean> subscriber) {
         subscribers.add(subscriber);
     }
 
