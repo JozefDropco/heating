acf34f17f8ee11ef71cebd1c9cacecf56d364fac
Jozef Dropco <Jozef_Dropco@swissre.com>
2021-05-31 15:19:56 +0200

    test

--- src/main/java/org/dropco/smarthome/solar/move/CountDownWatcher.java
+++ src/main/java/org/dropco/smarthome/solar/move/CountDownWatcher.java
@@ -10,8 +10,10 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.logging.Logger;
 
 public class CountDownWatcher {
+    private static final Logger LOGGER = Logger.getLogger(CountDownWatcher.class.getName());
     private AtomicInteger actualTicks = new AtomicInteger(0);
     private AtomicReference<RemovableGpioPinListenerDigital> listener = new AtomicReference<>();
 
@@ -24,8 +26,12 @@ public class CountDownWatcher {
     public void start(Consumer<Integer> callbackOnceFinished, Function<Consumer<Boolean>, RemovableGpioPinListenerDigital> realTimeTickerGetter, Consumer<BiConsumer<Supplier<Boolean>, Boolean>> movement, Supplier<Boolean> isMoving) {
         Consumer<Consumer<Void>> stopListener = unlinker -> {
             RemovableGpioPinListenerDigital listener = this.listener.get();
-            if (listener != null) listener.unlink();
+            if (listener != null) {
+                LOGGER.fine(CountDownWatcher.this + " Unlinking");
+                listener.unlink();
+            }
             callbackOnceFinished.accept(actualTicks.get());
+            LOGGER.fine(CountDownWatcher.this + " Unlinking");
             unlinker.accept(null);
         };
         SolarPanelManager.addStopListener(stopListener);
@@ -33,22 +39,28 @@ public class CountDownWatcher {
             if (state) {
                 int currentTickCount = actualTicks.incrementAndGet();
                 if (currentTickCount == tickCount) {
+                    LOGGER.fine(CountDownWatcher.this + " Unlinking 1");
                     listener.get().unlink();
                     callbackOnceFinished.accept(currentTickCount);
                 }
             }
         }));
-        movement.accept((unlink,moving) -> {
+        movement.accept((unlink, moving) -> {
             if (!moving) {
                 listener.get().unlink();
                 unlink.get();
+                LOGGER.fine(CountDownWatcher.this + " Unlinking 2");
                 callbackOnceFinished.accept(actualTicks.get());
             }
         });
         GpioFactory.getExecutorServiceFactory().getScheduledExecutorService().schedule(() -> {
-            if (!isMoving.get()){
+            if (!isMoving.get()) {
                 RemovableGpioPinListenerDigital listener = this.listener.get();
-                if (listener!=null) listener.unlink();
+                if (listener != null) {
+                    listener.unlink();
+
+                    LOGGER.fine(CountDownWatcher.this + " Unlinking 3");
+                }
                 SolarPanelManager.removeStopListener(stopListener);
                 callbackOnceFinished.accept(actualTicks.get());
             }
--- src/main/java/org/dropco/smarthome/solar/move/SolarPanelMover.java
+++ src/main/java/org/dropco/smarthome/solar/move/SolarPanelMover.java
@@ -1,5 +1,6 @@
 package org.dropco.smarthome.solar.move;
 
+import com.google.common.collect.ImmutableMap;
 import org.dropco.smarthome.gpioextension.RemovableGpioPinListenerDigital;
 import org.dropco.smarthome.solar.dto.AbsolutePosition;
 import org.dropco.smarthome.solar.dto.DeltaPosition;
@@ -9,6 +10,7 @@ import org.dropco.smarthome.solar.dto.PositionProcessor;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -22,10 +24,10 @@ import static org.dropco.smarthome.solar.SolarSystemRefCode.*;
 public class SolarPanelMover implements Runnable {
 
     private static final Logger LOGGER = Logger.getLogger(SolarPanelMover.class.getName());
-    protected static final String WEST = "Západ";
-    protected static final String EAST = "Východ";
-    protected static final String SOUTH = "Juh";
-    protected static final String NORTH = "Sever";
+    private static final Map<String, String> translationMap = ImmutableMap.of(WEST_PIN_REF_CD,"Západ",
+                                                                              EAST_PIN_REF_CD, "Východ",
+                                                                              NORTH_PIN_REF_CD, "Sever",
+                                                                              SOUTH_PIN_REF_CD, "Juh");
     private static Supplier<AbsolutePosition> currentPositionSupplier;
     private static BiConsumer<String, Boolean> commandExecutor;
     private static List<PositionChangeListener> listeners = Collections.synchronizedList(new ArrayList<>());
@@ -55,19 +57,18 @@ public class SolarPanelMover implements Runnable {
             }
         });
         int absHorizontal = abs(diffHorizontal);
-        int absVertical = abs(diffVertical);
-        boolean movingNorth = diffVertical < 0;
-        setState(NORTH_PIN_REF_CD, movingNorth && diffVertical != 0, NORTH);
-        setState(SOUTH_PIN_REF_CD, !movingNorth && diffVertical != 0, SOUTH);
-        boolean movingWest = diffHorizontal < 0;
-        setState(WEST_PIN_REF_CD, movingWest && diffHorizontal != 0, WEST);
-        setState(EAST_PIN_REF_CD, !movingWest && diffHorizontal != 0, EAST);
-
-        if (absVertical > 0) {
-            addVertical(currentPositionSupplier.get(), absVertical, movingNorth);
-        }
         if (absHorizontal > 0) {
+            boolean movingWest = diffHorizontal < 0;
+            setState(WEST_PIN_REF_CD, movingWest);
+            setState(EAST_PIN_REF_CD, !movingWest);
             addHorizontal(currentPositionSupplier.get(), absHorizontal, movingWest);
+        }
+        int absVertical = abs(diffVertical);
+        if (absVertical > 0) {
+            boolean movingNorth = diffVertical < 0;
+            setState(NORTH_PIN_REF_CD, movingNorth);
+            setState(SOUTH_PIN_REF_CD, !movingNorth);
+            addVertical(currentPositionSupplier.get(), absVertical, movingNorth);
         }
     }
 
@@ -75,10 +76,10 @@ public class SolarPanelMover implements Runnable {
         addWatch(absVertical, ticks -> {
             if (movingNorth) {
                 currentPosition.setVertical(currentPosition.getVertical() - ticks);
-                setState(NORTH_PIN_REF_CD, false, NORTH);
+                setState(NORTH_PIN_REF_CD, false);
             } else {
                 currentPosition.setVertical(currentPosition.getVertical() + ticks);
-                setState(SOUTH_PIN_REF_CD, false, SOUTH);
+                setState(SOUTH_PIN_REF_CD, false);
             }
             fireUpdate(currentPosition);
         }, VerticalMoveFeedback.getInstance()::addRealTimeTicker, VerticalMoveFeedback.getInstance()::addSubscriber,
@@ -89,10 +90,10 @@ public class SolarPanelMover implements Runnable {
         addWatch(absHorizontal, ticks -> {
             if (movingWest) {
                 currentPosition.setHorizontal(currentPosition.getHorizontal() - ticks);
-                setState(WEST_PIN_REF_CD, false, WEST);
+                setState(WEST_PIN_REF_CD, false);
             } else {
                 currentPosition.setHorizontal(currentPosition.getHorizontal() + ticks);
-                setState(EAST_PIN_REF_CD, false, EAST);
+                setState(EAST_PIN_REF_CD, false);
             }
             fireUpdate(currentPosition);
         }, HorizontalMoveFeedback.getInstance()::addRealTimeTicker, HorizontalMoveFeedback.getInstance()::addSubscriber,
@@ -103,11 +104,11 @@ public class SolarPanelMover implements Runnable {
         new CountDownWatcher(ticks).start(onceFinished, addRealTimeTicker, addMoveListener,isMoving);
     }
 
-    void setState(String pinRefCd, boolean state, String direction) {
+    void setState(String pinRefCd, boolean state) {
         if (state)
-            LOGGER.log(Level.INFO, "Natáčam na " + direction + ".");
+            LOGGER.log(Level.INFO, "Natáčam na " + translationMap.get(pinRefCd) + ".");
         else
-            LOGGER.log(Level.INFO, "Zastavujem otáčanie na " + direction + ".");
+            LOGGER.log(Level.INFO, "Zastavujem otáčanie na " + translationMap.get(pinRefCd) + ".");
         commandExecutor.accept(pinRefCd, state);
     }
 
