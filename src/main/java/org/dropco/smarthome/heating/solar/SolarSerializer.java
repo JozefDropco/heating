4b08469174bed399ffc09eb703535bb06ccf3c83
Jozef Dropco <Jozef_Dropco@swissre.com>
2022-01-19 09:46:08 +0100

    rework of solar

--- src/main/java/org/dropco/smarthome/heating/solar/SolarSerializer.java
+++ src/main/java/org/dropco/smarthome/heating/solar/SolarSerializer.java
@@ -5,12 +5,14 @@ import com.google.gson.GsonBuilder;
 import org.dropco.smarthome.RuntimeTypeAdapterFactory;
 import org.dropco.smarthome.heating.solar.dto.AbsolutePosition;
 import org.dropco.smarthome.heating.solar.dto.DeltaPosition;
+import org.dropco.smarthome.heating.solar.dto.ParkPosition;
 import org.dropco.smarthome.heating.solar.dto.Position;
 
 public class SolarSerializer {
    private static Gson gson = new GsonBuilder().registerTypeAdapterFactory(RuntimeTypeAdapterFactory.of(Position.class, "type", false)
             .registerSubtype(DeltaPosition.class, "delta")
-            .registerSubtype(AbsolutePosition.class, "absolute")).create();
+            .registerSubtype(AbsolutePosition.class, "absolute")
+            .registerSubtype(ParkPosition.class, "parking")).create();
 
     /***
      * Gets the gson
