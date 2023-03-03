b87caa751f207e8a92b35b9d6556d21c76683f70
Jozef Dropco <Jozef_Dropco@swissre.com>
2023-03-03 10:53:01 +0100

    boiler block added

--- src/main/java/org/dropco/smarthome/heating/heater/BoilerBlockerRelay.java
+++ src/main/java/org/dropco/smarthome/heating/heater/BoilerBlockerRelay.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 public class BoilerBlockerRelay {
     public static final String BOILER_BLOCK_PIN7 = "BOILER_BLOCK_PIN7";
     public static final String BOILER_BLOCK_PIN8 = "BOILER_BLOCK_PIN8";
+    private static final Logger logger = Logger.getLogger(BoilerBlockerRelay.class.getName());
     private ReentrantLock lock = new ReentrantLock(true);
     private ExecutorService executorService = GpioFactory.getExecutorServiceFactory().getScheduledExecutorService();
     private BiConsumer<String, Boolean> commandExecutor;
@@ -25,11 +26,13 @@ public class BoilerBlockerRelay {
             lock.lock();
             try {
                 commandExecutor.accept(BOILER_BLOCK_PIN7, true);
+                logger.log(Level.INFO, "Nastavujem PIN7 na 1");
                 Thread.sleep(1000);
                 commandExecutor.accept(BOILER_BLOCK_PIN7, false);
+                logger.log(Level.INFO, "Nastavujem PIN7 na 0");
 
             } catch (InterruptedException e) {
-                Logger.getLogger(BoilerBlockerRelay.class.getName()).log(Level.SEVERE, "Sleep interrupted");
+                logger.log(Level.SEVERE, "Sleep interrupted");
             } finally {
                 lock.unlock();
             }
@@ -41,14 +44,18 @@ public class BoilerBlockerRelay {
             lock.lock();
             try {
                 commandExecutor.accept(BOILER_BLOCK_PIN8, true);
+                logger.log(Level.INFO, "Nastavujem PIN8 na 1");
                 Thread.sleep(1000);
                 commandExecutor.accept(BOILER_BLOCK_PIN7, true);
+                logger.log(Level.INFO, "Nastavujem PIN7na 1");
                 Thread.sleep(1000);
                 commandExecutor.accept(BOILER_BLOCK_PIN7, false);
+                logger.log(Level.INFO, "Nastavujem PIN7 na 0");
                 Thread.sleep(1000);
                 commandExecutor.accept(BOILER_BLOCK_PIN8, false);
+                logger.log(Level.INFO, "Nastavujem PIN8 na 0");
             } catch (InterruptedException e) {
-                Logger.getLogger(BoilerBlockerRelay.class.getName()).log(Level.SEVERE, "Sleep interrupted");
+                logger.log(Level.SEVERE, "Sleep interrupted");
             } finally {
                 lock.unlock();
             }
