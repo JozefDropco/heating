98a5b4c683c4e9728e6564b6758665e26b72b7ea
JozefDropco <jozef.dropco@gmail.com>
2025-08-21 21:46:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 sudo -s
-wget https://project-downloads.drogon.net/wiringpi-latest.deb
-dpkg -i wiringpi-latest.deb
+echo "Advanced settings -> Wayland na X11"
+raspi-config
+wget https://github.com/WiringPi/WiringPi/releases/download/3.16/wiringpi_3.16_armhf.deb
+dpkg -i wiringpi_3.16_armhf.deb
 apt-get install aptitude
 apt-get install mc
 apt-get install openjdk-8-jdk
 apt-get install maven
 apt-get install realvnc-vnc-server
-apt-get install maria-db
-apg-get install phpmyadmin
+apt-get install mariadb-server
+apt-get install phpmyadmin
 git clone https://github.com/JozefDropco/heating.git
 mvn clean package -DskipTests=true
 #zapnut VNC v konfiguracii a one wire
 # do rc.local dopisat javu jar java -jar -Dhtml='/home/pi/heating/resources' target/heating-jar-with-dependencies.jar
 # do /home/pi/autostart pridat odkaz na chrome
 #
+echo """# /etc/systemd/system/rc-local.service
+[Unit]
+ Description=/etc/rc.local Compatibility
+ ConditionPathExists=/etc/rc.local
+
+[Service]
+ Type=forking
+ ExecStart=/etc/rc.local start
+ TimeoutSec=0
+ StandardOutput=tty
+ RemainAfterExit=yes
+ SysVStartPriority=99
+
+[Install]
+ WantedBy=multi-user.target""" > /etc/systemd/system/rc-local.service
+
+echo """
+java  -Dhtml='/home/jodido/heating/resources' -jar /home/jodido/heating/target/heating-jar-with-dependencies.jar --heating --solar & > /var/log/heating 
+exit 0
+""" >/etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo systemctl enable rc-local
